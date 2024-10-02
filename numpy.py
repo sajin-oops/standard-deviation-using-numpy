ed2fd91e01bdf6d8ff698e0e5a9530a44deeda94
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-10-02 21:45:30 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+arr = [20,20,20,20,20]
+a = np.mean(arr)
+print(a)
+
+'''
+20.0
+'''
+
+
+# to find standard deviation
+import numpy as np
+arr = [20,20,20,20,20]
+a = np.std(arr)
+print(a)
+
+'''
+0.0
+'''
